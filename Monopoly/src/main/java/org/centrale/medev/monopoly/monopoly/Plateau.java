45ccda2a4452e35188c0d06c631a8c5b7b2a1102
Mathys INFOSI <56838862+Wygoo@users.noreply.github.com>
2025-11-10 15:15:03 +0100

    plateau

--- Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/Plateau.java
+++ Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/Plateau.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.centrale.medev.monopoly.monopoly;
+import java.util.Random;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -14,6 +15,22 @@ public class Plateau {
     
     private ArrayList<Case> plateau;
     private LinkedList<Joueur> joueurs;
+    
+    public Plateau(){ 
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public int lancerDé(){
+        Random randInt = new Random();
+        int dé1 = randInt.nextInt(6)+1;
+        int dé2 = randInt.nextInt(6)+1;
+        int t = dé1+dé2;
+        
+        return t;
+    }
     
     public void supprimerJoueur(Joueur j){
         joueurs.remove(j);
