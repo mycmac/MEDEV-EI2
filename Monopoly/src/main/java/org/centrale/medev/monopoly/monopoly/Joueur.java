b4aa198c6d91284eb4c6b02276ea1b030e60d7b4
JustineSlr <sellierjustine05@gmail.Com>
2025-11-10 14:23:34 +0100

    Changed a method from private to public

---
+++ Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/Joueur.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.medev.monopoly.monopoly;
+
+/**
+ *
+ * @author selli
+ */
+public class Joueur {
+    String nom;
+    int fortune;
+    int position;
+    Plateau plateau;
+
+    
+    // 
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getFortune() {
+        return fortune;
+    }
+
+    public void setFortune(int fortune) {
+        this.fortune = fortune;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public Plateau getPlateau() {
+        return plateau;
+    }
+
+    public void setPlateau(Plateau plateau) {
+        this.plateau = plateau;
+    }
+    
+    
+    
+}
